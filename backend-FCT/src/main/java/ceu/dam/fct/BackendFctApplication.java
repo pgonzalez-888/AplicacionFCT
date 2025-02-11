4bd05ac686e0a63af6399950e6087a2ebea7b854
pablo <chorisochorisito@gmail.com>
2025-02-11 20:58:31 +0100

    Creación de los proyectos en eclipse.

---
+++ backend-FCT/src/main/java/ceu/dam/fct/BackendFctApplication.java
@@ -0,0 +1,13 @@
+package ceu.dam.fct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFctApplication.class, args);
+	}
+
+}
---
+++ backend-FCT/src/test/java/ceu/dam/fct/BackendFctApplicationTests.java
@@ -0,0 +1,13 @@
+package ceu.dam.fct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
