9fccfb001db09b43d0f41fc988d59ee405eab5c8
Vickytoria <vgordillo7875@ceuandalucia.es>
2025-02-14 17:14:20 +0100

    Solo falta redimensionar y los controllers

--- frontend-FCT/src/main/java/ceu/dam/javafx/proyectofct/App.java
+++ frontend-FCT/src/main/java/ceu/dam/javafx/proyectofct/App.java
@@ -1,5 +1,28 @@
 package ceu.dam.javafx.proyectofct;
 
-public class App {
+import ceu.dam.javafx.proyectofct.gui.AppController;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class App extends Application {
+
+	public static void main(String[] args) {
+		launch();
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.initStyle(StageStyle.UNDECORATED);
+		AppController controller = new AppController(primaryStage);
+		
+		primaryStage.setWidth(800);  // Ancho
+	    primaryStage.setHeight(600); // Alto
+	    
+		controller.changeScene(AppController.FXML_LOGIN);
+		primaryStage.initStyle(StageStyle.UNDECORATED);
+		primaryStage.show();
+
+	}
 
 }
---
+++ frontend-FCT/src/main/java/ceu/dam/javafx/proyectofct/gui/LoginController.java
@@ -0,0 +1,24 @@
+package ceu.dam.javafx.proyectofct.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController extends AppController{
+
+	 @FXML
+	    private Button btnIniciarSesion;
+
+	    @FXML
+	    private TextField tfPassword;
+
+	    @FXML
+	    private TextField tfUsername;
+
+	    @FXML
+	    void login(ActionEvent event) {
+	    	
+	    }
+
+}
