cbdd23bd59d1fe0665f30731e2b8ec2c72e8fbc7
Vickytoria <vgordillo7875@ceuandalucia.es>
2025-02-24 16:51:07 +0100

    Pantalla login

--- frontend-FCT/src/main/java/ceu/dam/javafx/proyectofct/gui/LoginController.java
+++ frontend-FCT/src/main/java/ceu/dam/javafx/proyectofct/gui/LoginController.java
@@ -1,35 +1,66 @@
 package ceu.dam.javafx.proyectofct.gui;
 
+import java.util.Optional;
+
+import org.openapitools.client.ApiClient;
 import org.openapitools.client.ApiException;
 import org.openapitools.client.api.UsuarioApiServiceApi;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
-public class LoginController extends AppController{
-	
+public class LoginController extends AppController {
+
 	private UsuarioApiServiceApi service;
 
-    @FXML
-    private PasswordField PasswordPassField;
+	@FXML
+	private Button cerrarAplicacionBtn;
 
-    @FXML
-    private Button btnIniciarSesion;
+	@FXML
+	private PasswordField PasswordPassField;
 
-    @FXML
-    private TextField tfUsername;
+	@FXML
+	private Button btnIniciarSesion;
 
-    @FXML
-    void login(ActionEvent event) {
-    	try {
+	@FXML
+	private TextField tfUsername;
+
+	@FXML
+	void login(ActionEvent event) {
+		try {
 			service.login(tfUsername.getText(), PasswordPassField.getText());
 		} catch (ApiException e) {
-			
+
 			e.printStackTrace();
 		}
-    }
+	}
+
+	@FXML
+	void salir(ActionEvent event) {
+		Alert pregunta = new Alert(AlertType.CONFIRMATION);
+		pregunta.setContentText("¿Estás seguro de querer cerrar?");
+		pregunta.setTitle("Confirmación");
+		pregunta.setHeaderText(null);
+		Optional<ButtonType> respuesta = pregunta.showAndWait();
+		if (respuesta.get() == ButtonType.OK) {
+			System.exit(0);
+		}
+
+	}
+	
+	@FXML
+	void initialize() {
+		ApiClient cliente = new ApiClient();
+		cliente.setApiKey("javiylasardillas");
+		cliente.setBasePath("http://localhost:8080");
+		service = new UsuarioApiServiceApi(cliente);
+		
+	}
 
 }
